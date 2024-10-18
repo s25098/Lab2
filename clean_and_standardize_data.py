b9fd5c5be62aa008f5e1cc6ecb3dc0dd9c76b5ac
s25098 <s25098@pjwstk.edu.pl>
2024-10-18 23:49:17 +0200

    Added generate_report method

--- clean_and_standardize_data.py
+++ clean_and_standardize_data.py
@@ -2,18 +2,18 @@ import pandas as pd
 import numpy as np
 
 total_rows = 0
-rows_removed = 0
-rows_modified = 0
+rows_deleted = 0
+rows_changed = 0
 
 
 def clean_data(path):
-    global total_rows, rows_removed, rows_modified
+    global total_rows, rows_deleted, rows_changed
 
     df = pd.read_csv('output_data.csv')
 
     total_rows = len(df)
 
-    rows_removed = df[df.isnull().sum(axis=1) >= 3].shape[0]
+    rows_deleted = df[df.isnull().sum(axis=1) >= 3].shape[0]
     df = df.dropna(thresh=len(df.columns) - 2)
 
     for i in df.columns:
@@ -23,13 +23,13 @@ def clean_data(path):
                 missing = df[i].isnull().sum()
                 df[i].fillna(median, inplace=True)
                 if missing > 0:
-                    rows_modified += missing
+                    rows_changed += missing
             else:
                 most_popular_value = df[i].mode()[0]
                 missing = df[i].isnull().sum()
                 df[i].fillna(most_popular_value, inplace=True)
                 if missing > 0:
-                    rows_modified += missing
+                    rows_changed += missing
 
     return df
 
@@ -41,8 +41,29 @@ def standardize_data(df):
     return df
 
 
+def generate_report():
+    total_deleted_percentage = (rows_deleted / total_rows * 100) if total_rows > 0 else 0
+    total_changed_percentage = (rows_changed / total_rows * 100) if total_rows > 0 else 0
+
+    report = f"""
+    Report:
+
+    Total number of Rows: {total_rows}
+    Deleted rows: {rows_deleted} ({total_deleted_percentage:.2f}%)
+    Changed rows: {rows_changed} ({total_changed_percentage:.2f}%)
+    """
+
+    with open('report.txt', 'w') as f:
+        f.write(report)
+
+    print(report)
+
+
 path = 'output_data.csv'
 df_clean = clean_data(path)
+
 df_standardize = standardize_data(df_clean)
 
 df_standardize.to_csv('clean_data.csv', index=False)
+
+generate_report()
